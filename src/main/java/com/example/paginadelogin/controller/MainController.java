1e5bdc3bab65f581b77e5a030d38b7abd9f79fbd
Isabella <0001166316@senaimgaluno.com.br>
2026-02-10 10:22:30 -0300

    Alterações no menu

--- src/main/java/com/example/paginadelogin/controller/MainController.java
+++ src/main/java/com/example/paginadelogin/controller/MainController.java
@@ -3,6 +3,9 @@ package com.example.paginadelogin.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.event.ActionEvent;
 
 
 public class MainController {
@@ -10,6 +13,10 @@ public class MainController {
     @FXML
     private BorderPane root;
 
+    @FXML
+    public void initialize() {
+        carregarTela("Home.fxml");
+    }
 
     @FXML
     public void abrirHome() {
@@ -24,6 +31,20 @@ public class MainController {
     @FXML
     public void abrirClientes() {
         carregarTela("Clientes.fxml");
+    }
+
+    public void abrirAjuda(ActionEvent event) {
+
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Sobre o sistema");
+        alert.setHeaderText("Informações do sistema");
+        alert.setContentText(
+                "Nome: Sistema de Reservas\n" +
+                        "Versão: 1.0\n" +
+                        "Desenvolvedor: Isabella Carmélia\n" +
+                        "Ano: 2026"
+        );
+        alert.showAndWait();
     }
 
     private void carregarTela(String fxml) {
